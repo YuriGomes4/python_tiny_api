347e3834750649fc2f98b45c58a122c3bd686b15
Yuri Gomes <73554025+YuriGomes4@users.noreply.github.com>
2025-07-31 23:29:35 -0300

    att

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as arq:
     readme = arq.read()
 
 setup(name='py_tiny',
-    version='0.0.4',
+    version='0.0.5',
     license='MIT License',
     author='Yuri Gomes',
     long_description=readme,
     long_description_content_type="text/markdown",
     author_email='yurialdegomes@gmail.com',
-    keywords='clickup',
+    keywords='tiny',
     description=u'Wrapper não oficial do Tiny V2',
     packages=['py_tiny'],
     install_requires=['requests'],)
